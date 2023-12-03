59969cc8198aca2b0fe9d5c1b4986e1996b70fd1
zaki <codezak75@gmail.com>
2023-12-03 01:25:12 +0100

    finishing the project

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
 const date_picker_elem = document.querySelector('.date-picker');
 const selected_date = document.querySelector('.date-picker .selected-date');
 const dates_elem = document.querySelector('.date-picker .dates');
-const days = document.querySelector('.days');
+const month_elem = document.querySelector('.date-picker .dates .month .mth');
+const prv_month = document.querySelector('.date-picker .dates .month .prev-mth');
+const nxt_month = document.querySelector('.date-picker .dates .month .next-mth');
+const days_elem = document.querySelector('.days');
 
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+let date = new Date();
+let day = date.getDate();
+let month = date.getMonth();
+let year = date.getFullYear();
+console.log(day);
 
+let selectedDate = date;
+let selectedDay = day;
+let selectedMonth = month;
+let selectedYear = year;
 
+month_elem.textContent = months[month] + ' '+ year;
+selected_date.textContent = formatDate(date);
+selected_date.dataset.value = selectedDate;
+
+populateday();
 date_picker_elem.addEventListener('click',toggleDatePicker);
+nxt_month.addEventListener('click',goToNextMonth);
+prv_month.addEventListener('click',goToPrevMonth);
 
 function toggleDatePicker(e){
     if(!checkClass(e.composedPath(),'dates')){
@@ -22,14 +42,69 @@ function checkClass(path,checker){
     return false;
 }
 
-
-// days appending nades
-const dayP = document.createElement('div');
-// days.appendChild(dayP)
-for(let i=0;i<30;i++){
-    var day = document.createElement('div');
-    day.innerHTML = i+1;
-    day.classList.add('day');
-    days.appendChild(day);
+function goToNextMonth(){
+    month ++;
+    if(month > 11){
+        month = 0;
+        year++;
+    }
+    month_elem.textContent = months[month] +" "+ year;
+    populateday();
 }
-console.log(days.childNodes)
+function goToPrevMonth(){
+    month --;
+    if(month < 0){
+        month = 11;
+        year--
+    }
+    month_elem.textContent = months[month] +" "+ year;
+    populateday();
+}
+
+function formatDate(d){
+    let day = d.getDate();
+    if(day<10){
+        day = `0${day}`;
+    }
+    let month = d.getMonth();
+    if(month<10){
+        month = `0${month}`;
+    }
+    let year = d.getFullYear();
+
+    return `${day} - ${month} - ${year}`;
+}
+// days appending nodes
+
+function populateday(e){
+    days_elem.innerHTML = "";
+    let amount_days = 30;
+    if(month === 1){
+        amount_days = 28;
+    }else if(month % 2){
+        amount_days = 31;
+    }
+
+    for(let m=0;m<amount_days;m++){
+        var day_div = document.createElement('div');
+        day_div.textContent = m+1;
+        day_div.classList.add('day');
+        days_elem.appendChild(day_div);
+        
+        if(selectedDay == m+1 && selectedMonth == month && selectedYear == year){
+            console.log(selectedYear);
+            day_div.classList.add("selected");
+        }
+
+        day_div.addEventListener('click',()=>{ 
+            selectedDate = new Date(year+ "-" + (month + 1) + "-" + (m + 1));
+            selectedDay = m+1;
+            selectedMonth = month;
+            selectedYear = year;
+
+            selected_date.textContent = formatDate(selectedDate);
+            selected_date.dataset.value = selectedDate;
+            populateday();
+        })
+    }
+}
